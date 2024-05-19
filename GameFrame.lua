61426636168474ce59dcd8afe4404a51cad0f048
zhizhiyiyi <704214015@qq.com>
2024-05-19 17:44:30 +0800

    引入物理效果，测试版

--- GameFrame.lua
+++ GameFrame.lua
@@ -1,4 +1,5 @@
 require("tools/Frame");
+require("tools/HexColor");
 
 GameFrame = Frame:new();
 
@@ -10,10 +11,16 @@ function GameFrame:new(name, backColor, buttonArray, isEnable)
     return ret;
 end
 
-function GameFrame:draw()
+function GameFrame:draw(objects)
     if self.isEnable == false then
         return;
     end
     love.graphics.clear(self.backColor);
+    love.graphics.setColor(HexColor("#000000"));
+    local windowWidth, windowHeight = love.graphics.getDimensions();
+    love.graphics.line(250, 0, 250, windowHeight);
     self.buttonArray:draw();
+
+    love.graphics.setColor(HexColor("#238923"));
+    love.graphics.polygon("fill", objects.block.body:getWorldPoints(objects.block.shape:getPoints()));
 end
--- main.lua
+++ main.lua
@@ -49,6 +49,43 @@ function createGameFrame(frameBackColor)
     return gameFrame;
 end
 
+function createPhysicsWorld()
+    love.physics.setMeter(64);
+    world = love.physics.newWorld(0, 9.81 * 64, true);
+    objects = {};
+
+    local windowWidth, windowHeight = love.graphics.getDimensions();
+    
+    -- 设置一个矩形边界，所有物理模拟均限制在此举行中
+    objects.downBorder = {};
+    objects.downBorder.body = love.physics.newBody(world, 0, 0, "static");
+    objects.downBorder.shape = love.physics.newEdgeShape(0, windowHeight, windowWidth, windowHeight);
+    objects.downBorder.fixture = love.physics.newFixture(objects.downBorder.body, objects.downBorder.shape);
+    objects.downBorder.fixture:setFriction(0.3);
+
+    objects.leftBorder = {};
+    objects.leftBorder.body = love.physics.newBody(world, 0, 0, "static");
+    objects.leftBorder.shape = love.physics.newEdgeShape(0, 0, 0, windowHeight);
+    objects.leftBorder.fixture = love.physics.newFixture(objects.leftBorder.body, objects.leftBorder.shape);
+
+    objects.rightBorder = {};
+    objects.rightBorder.body = love.physics.newBody(world, 0, 0, "static");
+    objects.rightBorder.shape = love.physics.newEdgeShape(windowWidth, 0, windowWidth, windowHeight);
+    objects.rightBorder.fixture = love.physics.newFixture(objects.rightBorder.body, objects.rightBorder.shape);
+
+    objects.upBorder = {};
+    objects.upBorder.body = love.physics.newBody(world, 0, 0, "static");
+    objects.upBorder.shape = love.physics.newEdgeShape(0, 0, windowWidth, 0);
+    objects.upBorder.fixture = love.physics.newFixture(objects.upBorder.body, objects.upBorder.shape);
+
+    objects.block = {};
+    objects.block.body = love.physics.newBody(world, 650 / 2, 650 / 2, "dynamic");
+    objects.block.shape = love.physics.newRectangleShape(0, 0, 100, 100);
+    objects.block.fixture = love.physics.newFixture(objects.block.body, objects.block.shape, 1);
+    objects.block.fixture:setRestitution(0.5);
+    objects.block.fixture:setFriction(0.3);
+end
+
 function love.load()
     love.graphics.setBackgroundColor(frameBackColor);
 
@@ -56,6 +93,7 @@ function love.load()
     menuFrame = createMenuFrame(frameBackColor);
     -- 游戏界面
     gameFrame = createGameFrame(frameBackColor);
+    createPhysicsWorld();
     -- 当前界面
     currFrame = menuFrame;
 
@@ -63,7 +101,16 @@ function love.load()
 end
 
 function love.update(dt)
-
+    world:update(dt)
+    if love.keyboard.isDown("right") then
+        objects.block.body:applyForce(4000, 0)
+    end
+    if love.keyboard.isDown("left") then
+        objects.block.body:applyForce(-4000, 0)
+    end
+    if love.keyboard.isDown("up") then
+        objects.block.body:applyForce(0, -4000)
+    end
 end
 
 function love.mousemoved(x, y)
@@ -82,5 +129,5 @@ function love.mousepressed(x, y)
 end
 
 function love.draw()
-    currFrame:draw();
+    currFrame:draw(objects);
 end
